aa41aefd5104a8032962a052e046a16ca1e12c9d
Ji Zhang <jizhang@anjuke.com>
2014-01-24 19:01:17 +0800

    custom authentication provider

---
+++ src/main/java/com/anjuke/dw/mini/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.anjuke.dw.mini.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public String login() {
+
+
+        return "abc";
+    }
+
+}
